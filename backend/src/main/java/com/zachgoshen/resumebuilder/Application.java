3dd31d0af97dbefeee496049724333df47affc38
Zachary Goshen <zachgoshen@gmail.com>
2020-12-31 11:53:59 -0500

    Add resumes/{id} route to front end and build formgroup for resume

--- backend/src/main/java/com/zachgoshen/resumebuilder/Application.java
+++ backend/src/main/java/com/zachgoshen/resumebuilder/Application.java
@@ -19,7 +19,7 @@ public class Application {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/resumes").allowedOrigins("http://localhost:4200");
+				registry.addMapping("/**");
 			}
 		};
 	}
